930d93b087a783ea5ec09ebb87a979aaa64293b9
Deep1218 <deeppatel18dec@gmail.com>
2022-05-20 14:46:52 +0530

    [feature] create account page

--- src/app/login-one/login-one.module.ts
+++ src/app/login-one/login-one.module.ts
@@ -4,16 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginOneRoutingModule } from './login-one-routing.module';
 import { LoginOneComponent } from './login-one.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
     LoginOneComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    CreateAccountComponent,
   ],
-  imports: [
-    CommonModule,
-    LoginOneRoutingModule
-  ]
+  imports: [CommonModule, LoginOneRoutingModule],
 })
-export class LoginOneModule { }
+export class LoginOneModule {}
