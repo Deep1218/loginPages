f89897a4f6cfb4d21c50c6f65de97d6dd81e1710
Deep1218 <deeppatel18dec@gmail.com>
2022-05-20 20:42:20 +0530

    [feature] confirm password and password validator added

--- src/app/login-one/create-account/create-account.component.ts
+++ src/app/login-one/create-account/create-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../providers/custom-validator';
 
 @Component({
   selector: 'app-create-account',
@@ -10,45 +11,49 @@ export class CreateAccountComponent implements OnInit {
   newAccount!: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.newAccount = this.fb.group({
-      fullName: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(30),
-          Validators.pattern('^[a-z A-Z]+$'), // Done
+    this.newAccount = this.fb.group(
+      {
+        fullName: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(30),
+            Validators.pattern('^[a-z A-Z]+$'), // Done
+          ],
         ],
-      ],
-      phoneNumber: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(10),
-          Validators.maxLength(13),
-          Validators.pattern(
-            '([+((0-9)]{1})(([(0-9)+() -.]){5,16})([+((0-9)]{1})'
-          ),
+        phoneNumber: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(13),
+            Validators.pattern(
+              '([+((0-9)]{1})(([(0-9)+() -.]){5,16})([+((0-9)]{1})'
+            ),
+          ],
         ],
-      ],
-      email: ['', [Validators.required, Validators.email]],
-      //- at least 8 characters
-      // - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
-      // - Can contain special characters
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(
-            '((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z]).{8,64})'
-          ),
+        email: ['', [Validators.required, Validators.email]],
+        //- at least 8 characters
+        // - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
+        // - Can contain special characters
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              '((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z]).{8,64})'
+            ),
+          ],
         ],
-      ],
-    });
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: CustomValidators.mustMatch('password', 'confirmPassword') }
+    );
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.newAccount.get('password'));
+    console.log(this.newAccount.get('confirmPassword'));
   }
 }
