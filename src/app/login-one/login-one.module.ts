87dc34d939ef01edcc541c15d4687faaa450790b
Deep1218 <deeppatel18dec@gmail.com>
2022-05-23 13:02:11 +0530

    [refactor]

--- src/app/login-one/login-one.module.ts
+++ src/app/login-one/login-one.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoginOneRoutingModule } from './login-one-routing.module';
 import { LoginOneComponent } from './login-one.component';
