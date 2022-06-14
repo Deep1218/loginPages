6046740684e5bda87d2eb2b34649b2ae211f9356
Deep1218 <deeppatel18dec@gmail.com>
2022-06-14 15:22:46 +0530

    [feature] convert components into module

--- src/app/login-one/login-one.module.ts
+++ src/app/login-one/login-one.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ComponentsModule } from '../components/components.module';
 
 import { LoginOneRoutingModule } from './login-one-routing.module';
 import { LoginOneComponent } from './login-one.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
-import { ToastComponent } from '../components/toast/toast.component';
 
 @NgModule({
   declarations: [
     LoginOneComponent,
     ForgotPasswordComponent,
     CreateAccountComponent,
-    ToastComponent,
   ],
   imports: [
     CommonModule,
     LoginOneRoutingModule,
     ReactiveFormsModule,
     NgbModule,
+    ComponentsModule,
   ],
 })
 export class LoginOneModule {}
