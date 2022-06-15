a92c6dd5fb767f82c11f5a3cfdd8c42283e0c914
Deep1218 <deeppatel18dec@gmail.com>
2022-06-15 17:49:56 +0530

    [feature] login two completed

--- src/app/login-two/login-two-routing.module.ts
+++ src/app/login-two/login-two-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginTwoComponent } from './login-two.component';
 
 const routes: Routes = [
   { path: '', component: LoginTwoComponent },
-  // { path: 'forgotPassword', component: ForgotPasswordComponent },
-  // { path: 'newAccount', component: CreateAccountComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'signup', component: CreateAccountComponent },
 ];
 
 @NgModule({
--- src/app/login-two/login-two.module.ts
+++ src/app/login-two/login-two.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LoginTwoRoutingModule } from './login-two-routing.module';
 import { LoginTwoComponent } from './login-two.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
   declarations: [
-    LoginTwoComponent
+    LoginTwoComponent,
+    CreateAccountComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
